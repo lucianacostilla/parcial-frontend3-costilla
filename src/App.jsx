936c3595ddff95ffd180ba684133701823594a44
lucianacostilla <costilalu@gmail.com>
2023-05-31 19:32:11 -0300

    agregando proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,38 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
- 
-  return (
-  <>
-  <div className='App'>
-    <h1>
-      Carga de Estudiantes
-    </h1>
-    <form>
-    </form>
-   <Card/>
-   </div>
-   </> 
+  const [nombre, setNombre] = useState('')
+  const [apellido, setApellido] = useState('')
+  const [error, setError] = useState('')
 
+  const handleOnChangeNombre = (e) => {setNombre(e.target.value)}
+  const handleOnChangeApellido = (e) => {setApellido(e.target.value)}
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!/^\S/.test(nombre)) {
+      setError('Verificar que no haya espacios antes del nombre.')
+    }
+    if (nombre.length < 3 || apellido.length < 6){
+      setError('El nombre debe tener al menos 3 caracteres y el apellido 6.')
+    }
+    else {
+      setError(null)
+    }
+  }
+return (
+<>
+    <div className='form'>
+   <h1> Cargando datos. </h1>
+   <form onSubmit={handleSubmit}>
+    <label htmlFor="nombre"> Escribe tu nombre: </label>
+    <input type="text" id="nombre" placeholder='Por ejemplo: Luciana' onChange={handleOnChangeNombre} />
+    <label htmlFor="apellido"> Escribe tu apellido: </label>
+    <input type="text" id="apellido" placeholder='Por ejemplo: Costilla' onChange={handleOnChangeApellido} />
+   <button className='btn'> Enviar </button>
+   </form>
+   {error === null ? <Card nombre={nombre} apellido={apellido} /> : <p className='err'>{error}</p>}
+   </div>
+    </>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({nombre, apellido}) => {
   return (
-    <div> <p> Hola, soy una tarjeta!</p> </div>
+    <div className='card'>
+      <h2>Hola {nombre} {apellido}!</h2>
+      <h3>Gracias por suscribirte y por confiar en nosotros. Pronto estará listo Exotify!</h3>
+    </div>
   )
 }
 
 export default Card
+
